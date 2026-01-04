6e77a6f8d85f899159431c9b64c2ae8e9e9177f7
Sandra <sandra.mstowskaa@gmail.com>
2026-01-04 16:15:31 +0100

    task01: feat: add routing in nav (home, contact)

--- 01/Task01.js
+++ 01/Task01.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import Home from "../src/components/Home";
+import Contact from "../src/components/Contact";
+import { Link, Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 
 const Task01 = () => {
-    return (
-        <h1>Task01</h1>
-    );
-}
+  return (
+    <div>
+      <h1>Task01</h1>
+      <nav>
+        <Link to="/task01/home">Home</Link> |{" "}
+        <Link to="/task01/contact">Contact</Link>
+      </nav>
+      <Switch>
+        <Route exact path="/task01/home" component={Home} />
+        <Route exact path="/task01/contact" component={Contact} />
+      </Switch>
+    </div>
+  );
+};
 
 export default Task01;
 
